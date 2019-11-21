b7bc0831eb6ce46129607e09ef0b4263d76aade9
Carlos Escobar <support@wsg.com.gt>
2019-11-20 18:00:28 -0600

    Examen final

--- pelicula/models.py
+++ pelicula/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib import admin
 
-# Create your models here.
+
+class Plato(models.Model):
+    nombre = models.CharField(max_length=30)
+    precio = models.IntegerField()
+
+    def _str_(self):
+        return self.nombre
+
+class Menu(models.Model):
+    nombre= models.CharField(max_length=30)
+    precio= models.IntegerField()
+    platos= models.ManyToManyField(Plato, through='Menus')
+    def _str_(self):
+        return self.nombre
+
+class Menus(models.Model):
+    plato = models.ForeignKey(Plato, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu,on_delete=models.CASCADE)
+
+
+class MenusInLine(admin.TabularInline):
+    model=Menus
+    extra=1
+
+class PlatoAdmin(admin.ModelAdmin):
+    inlines=(MenusInLine,)
+class MenuAdmin(admin.ModelAdmin):
+    inlines=(MenusInLine,)
--- pelicula/views.py
+++ pelicula/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .forms import MenuForm
+from pelicula.models import Menu, Menus
 # Create your views here.
+
+def ListaMenus(request):
+    menus = Menus.objects.all
+    return render(request, 'menus/ListaMenu.html', {'menus':menus})
+
+def menu_nuevo(request):
+    if request.method == "POST":
+        formulario = MenuForm(request.POST)
+        if formulario.is_valid():
+            menu = Menu.objects.create(nombre=formulario.cleaned_data['nombre'], precio=formulario.cleaned_data['precio'])
+            for plato_id in request.POST.getlist('platos'):
+                menus = Menus(plato_id=plato_id, menu_id = menu.id)
+                menus.save()
+            messages.add_message(request, messages.SUCCESS, 'Menu Guardado Exitosamente')
+
+    else:
+        formulario = MenuForm()
+    return render(request,'menus/menu_editar.html')
